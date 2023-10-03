197391217f67f5efa5622c40b860b279784b3330
naufalardhu <141219244+naufalardhu@users.noreply.github.com>
2023-10-03 10:01:03 +0700

    Add files via upload

---
+++ Naufal Ardhu Jarak Horizontal.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+// Headers
+string toString (double);
+int toInt (string);
+double toDouble (string);
+
+int main() {
+    double v;
+    double s;
+    double sinA;
+    double jarak;
+    
+    cin >> v;
+    cin >> s;
+    sinA = sin(2 * s * 3.141 / 180);
+    jarak = v * sinA / 10;
+    cout << jarak << endl;
+    return 0;
+}
+
+// The following implements type conversion functions.
+string toString (double value) { //int also
+    stringstream temp;
+    temp << value;
+    return temp.str();
+}
+
+int toInt (string text) {
+    return atoi(text.c_str());
+}
+
+double toDouble (string text) {
+    return atof(text.c_str());
+}
+
